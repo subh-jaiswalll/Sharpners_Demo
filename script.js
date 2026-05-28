8977a22f42b30ac75ec6de799ac352d088cb1f70
atulcodes-tech <subhkaranjaiswal41@gmail.com>
2026-05-28 13:55:24 +0530

    Intilized Commit

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 
+const head = document.getElementsByClassName('main-heading');
 
+const head1 = document.createElement('p');
+
+head1.innerHTML.
 function Start(event) {
     console.log("Button clicked!")
+
+
 }
