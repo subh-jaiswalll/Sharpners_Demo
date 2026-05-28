4ee3c21bd0b1b087abc45533649f6e0811125f15
atulcodes-tech <subhkaranjaiswal41@gmail.com>
2026-05-28 21:15:18 +0530

    ElementsByClassDone

---
+++ DOM/ElementByClass/script.js
@@ -0,0 +1,8 @@
+
+const fruits = document.getElementsByClassName("fruit");
+
+fruits[2].style.backgroundColor = "yellow";
+
+for(let i = 0; i < fruits.length; i++){
+    fruits[i].style.fontWeight = "bold";
+}
